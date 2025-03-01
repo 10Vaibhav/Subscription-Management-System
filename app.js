b54cfe418e52dc34031119f371dd51c70c175e19
Vaibhav Mahajan <143709642+10Vaibhav@users.noreply.github.com>
2025-03-01 22:52:47 +0530

    configure arject and integrate it with subscription management

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ import authRouter from "./routes/auth.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 import connectToDatabase from "./database/mongodb.js";
 import errorMiddleware from "./middlewares/error.middleware.js";
+import arcjectMiddleware from "./middlewares/arcject.middleware.js";
 
 const app = express();
 
@@ -15,12 +16,12 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 
-
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
 
 app.use(errorMiddleware);
+app.use(arcjectMiddleware);
 
 app.get("/", (req,res)=> {
     res.send("Welcome to the Subscription Tracker API !!");
---
+++ middlewares/arcject.middleware.js
@@ -0,0 +1,29 @@
+import aj from "../config/arcject.js"
+
+const arcjectMiddleware = async (req,res,next) => {
+
+    try{
+
+        const decision = await aj.protect(req, {requested: 1});
+
+        if(decision.isDenied()){
+            if(decision.reason.isRateLimit()){
+                return res.status(429).json({error: "Rate limit exceeded"});
+            }
+
+            if(decision.reason.isBot()){
+                return res.status(403).json({error: "Bot detected"});
+            }
+
+            return res.status(403).json({error: "Access denied"});
+        }
+
+        next();
+
+    }catch(error){
+        console.log("Arject Middleware error: ", error);
+        return res.status(500).json({error: "Protection service error"});
+    }
+}
+
+export default arcjectMiddleware;
